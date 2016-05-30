31c92a00ccace26dfa321e253d42236ff6e3b249
Tangtang <740306614@qq.com>
2016-05-30 17:57:22 +0800

    test App
    …..

--- SeasonalVegetables/SVXClasses/SVXMain/SVXItems/Home/SVXHomeController/SVXHomeViewController.m
+++ SeasonalVegetables/SVXClasses/SVXMain/SVXItems/Home/SVXHomeController/SVXHomeViewController.m
@@ -66,6 +66,8 @@ static int const kLineWidth = 50;
     [self p_setupChildViewController];
     [self p_setupTitle];
     
+    NSLog(@"hello");
+    
     self.containScroller.contentSize = CGSizeMake(self.childViewControllers.count * SVXWidth, 0);
     
 }
